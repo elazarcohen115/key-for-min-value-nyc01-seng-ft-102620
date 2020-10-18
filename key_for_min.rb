2ee017c737485ca255b42e05173b70b9abcd34ca
simon cohen <escohen115@gmail.com>
2020-10-18 17:20:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   lowest_value = nil
 
   name_hash.each do |k, v|
-    if v < lowest_value || lowest_value = nil
+    if v < lowest_value || lowest_value == nil
       lowest_value = v
       lowest_key = k
     end
